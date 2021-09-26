630155f6e0bf5f3456a1462865262aca6454f90f
Siebe Holvoet <55406650+SHolvoet@users.noreply.github.com>
2021-09-26 12:05:27 +0200

    Tutorial completed

---
+++ CommerceWebApp/eShop.UseCases/SearchProductScreen/SearchProduct.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using eShop.CoreBusiness.Models;
+using eShop.UseCases.PluginInterfaces.DataStore;
+
+namespace eShop.UseCases.SearchProductScreen
+{
+    public class SearchProduct : ISearchProduct
+    {
+        private readonly IProductRepository productRepository;
+
+        public SearchProduct(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public IEnumerable<Product> Execute(string filter = null)
+        {
+            return productRepository.GetProducts(filter);
+        }
+    }
+}
